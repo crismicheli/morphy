0073d720d279e1eaa343dddf8a064fb8316407bc
Cris Micheli <crismicheli.work@gmail.com>
2026-05-06 20:44:33 +0200

    Update imports for temporal classifier module

--- classifiers/classifier_dispatch.py
+++ classifiers/classifier_dispatch.py
@@ -2,8 +2,8 @@ from __future__ import annotations
 
 from .static_classifier  import STATE_COLORS as STATIC_STATE_COLORS
 from .static_classifier  import classify_state as classify_static_state
-from .temporal_static_classifier  import classify_state as classify_temporal_state
-from .temporal_static_classifier  import reset_classifier_memory as reset_temporal_memory
+from .temporal_classifier  import classify_state as classify_temporal_state
+from .temporal_classifier  import reset_classifier_memory as reset_temporal_memory
 from .state_machine_classifier import classify_state as classify_state_machine
 from .state_machine_classifier import reset_classifier_memory as reset_state_machine_memory
 
